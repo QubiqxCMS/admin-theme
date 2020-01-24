25860029515122ce02e33704fa7e8318575590d1
Robin van Maasakker <robin@qubiqx.com>
2020-01-24 08:20:35 +0100

    Update

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,19 +1,59 @@
-require('./includes/vue-loading-spinner');
-require('./includes/articles');
+// require('./includes/vue-loading-spinner');
+// require('./includes/articles');
 require('./includes/jquery-sortable');
-require('./includes/jscolor');
+// require('./includes/jscolor');
+//
+// //Theme
+// require('./includes/app.min');
+// require('./includes/actions');
+// require('./includes/functions/app');
+// require('./includes/functions/vendors');
+//
+// // require('./includes/bower_components/jquery/dist/jquery.min');
+// // require('./includes/bower_components/bootstrap/dist/js/bootstrap.min');
+import VueCharts from '../../../../node_modules/vue-chartjs'
 
-//Theme
-require('./includes/app.min');
-require('./includes/actions');
-require('./includes/functions/app');
-require('./includes/functions/vendors');
+window.VueChartJs = VueCharts;
+require('./includes/jquery.peity.min');
+require('./includes/tree.jquery');
+// require('./includes/bower_components/flatpickr/dist/flatpickr.min');
+//
+// require('./includes/datepicker');
+//
 
-// require('./includes/bower_components/jquery/dist/jquery.min');
-// require('./includes/bower_components/bootstrap/dist/js/bootstrap.min');
-require('./includes/bower_components/peity/jquery.peity.min');
-require('./includes/bower_components/jqTree/tree.jquery');
-require('./includes/bower_components/flatpickr/dist/flatpickr.min');
+$(document).ready(function () {
+    document.querySelector('.menu-button').addEventListener('click', () => {
+        document.querySelector('.sidebar-menu').classList.toggle('-translate-x-full')
+    });
+    document.querySelector('.menu-button-close').addEventListener('click', () => {
+        document.querySelector('.sidebar-menu').classList.toggle('-translate-x-full')
+    });
 
-require('./includes/datepicker');
-
+    $('.tab').on('click', function (event) {
+        var tab = $(event.target).attr('href');
+        if (!tab) {
+            return;
+        }
+        $('.tab-link.active').removeClass('active');
+        $(event.target).addClass('active');
+        $('.tab-pane').removeClass('active');
+        $(tab).addClass('active');
+    });
+    $('.tab2').on('click', function (event) {
+        var tab = $(event.target).attr('href');
+        if (!tab) {
+            return;
+        }
+        $('.tab-link2.active').removeClass('active');
+        $(event.target).addClass('active');
+        $('.tab-pane2').removeClass('active');
+        $(tab).addClass('active');
+    });
+    function slugify(str) {
+        var slug = '';
+        var trimmed = $.trim(str);
+        slug = trimmed.replace(/[^a-z0-9-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
+        return slug.toLowerCase();
+    }
+    window.slugify = slugify;
+});
---
+++ Assets/tailwind.config.js
@@ -0,0 +1,35 @@
+const {colors} = require('tailwindcss/defaultTheme');
+module.exports = {
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+        extend: {
+            colors: {
+                primary: 'var(--color-primary)',
+                secondary: 'var(--color-secondary)',
+            },
+            textColor: {
+                primary: 'var(--color-primary)',
+                secondary: 'var(--color-secondary)',
+            },
+            boxShadow: {
+                whitelight: '0 0px 25px -3px rgba(255, 255, 255, .1)'
+            },
+            borderWidth: {
+                20: '20px',
+                40: '40px',
+                50: '50px'
+            }
+        },
+    },
+    variants: {
+        borderColors: ["responsive", "hover", "focus", "group-hover"],
+        visibility: ["responsive", "group-hover"],
+    },
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]
+}
